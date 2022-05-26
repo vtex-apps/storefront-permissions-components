062e07454b80dc977c09416113d69ae6493410d2
polishq <arthur.bond@vtex.com.br>
2022-05-26 17:11:39 -0400

    [B2BORG-114] Autocomplete organization selector

--- react/components/customers-admin.tsx
+++ react/components/customers-admin.tsx
@@ -1,26 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { FC } from 'react'
 import React, { useState } from 'react'
-import type { WrappedComponentProps } from 'react-intl'
-import { injectIntl, defineMessages } from 'react-intl'
+import { useIntl, defineMessages } from 'react-intl'
 import { useQuery, useMutation, useLazyQuery } from 'react-apollo'
-import {
-  Button,
-  Dropdown,
-  Alert,
-  ButtonWithIcon,
-  IconClear,
-} from 'vtex.styleguide'
+import { Card, Button, Dropdown, Alert, Spinner } from 'vtex.styleguide'
 
+import OrganizationsAutocomplete from './OrganizationsAutocomplete'
 import GET_USER from '../queries/getUser.gql'
 import GET_ROLES from '../queries/ListRoles.gql'
-import GET_ORG from '../queries/listOrganizations.gql'
 import GET_COST from '../queries/costCentersByOrg.gql'
 import SAVE_USER from '../mutations/saveUser.gql'
-
-const remove = <IconClear />
+import DELETE_USER from '../mutations/deleteUser.gql'
 
 const messages = defineMessages({
+  b2bInfo: {
+    id: 'admin/storefront-permissions.b2bInfo.title',
+    defaultMessage: 'B2B Information',
+  },
   role: {
     id: 'admin/storefront-permissions.tab.roles.name.label',
     defaultMessage: 'Role',
@@ -57,6 +53,10 @@ const messages = defineMessages({
     id: 'admin/storefront-permissions.button.save',
     defaultMessage: 'Save',
   },
+  delete: {
+    id: 'admin/storefront-permissions.button.delete',
+    defaultMessage: 'Remove B2B Info',
+  },
   success: {
     id: 'admin/storefront-permissions.tab.users.success',
     defaultMessage: 'B2B info saved',
@@ -73,39 +73,60 @@ const messages = defineMessages({
 })
 
 const parseOptions = (options: any) => {
-  const ret =
-    options?.data.map((org: any) => {
+  return (
+    options?.data?.map((orgOrCostCenter: any) => {
       return {
-        value: org.id,
-        label: org.name,
+        value: orgOrCostCenter.id,
+        label: orgOrCostCenter.name,
       }
     }) ?? []
-
-  return ret
+  )
 }
 
-const UserEdit: FC<any & WrappedComponentProps> = (props: any) => {
-  const { intl, id, showName, showEmail, showCancel, onCancel, onSave } = props
+const initialState = {
+  message: null,
+  id: null,
+  roleId: null,
+  orgId: null,
+  costId: null,
+  userId: null,
+  clId: null,
+  name: null,
+  email: null,
+  canImpersonate: false,
+}
+
+const UserEdit: FC<any> = (props: any) => {
+  const { formatMessage } = useIntl()
+  const {
+    id,
+    name = null,
+    email = null,
+    showName,
+    showEmail,
+    showCancel,
+    onCancel,
+    onSave,
+  } = props
 
   const [state, setState] = useState<any>({
-    message: null,
-    id: null,
-    roleId: null,
-    orgId: null,
-    costId: null,
-    userId: null,
+    ...initialState,
     clId: id,
-    name: props.name ?? null,
-    email: props.email ?? null,
-    canImpersonate: false,
+    name,
+    email,
   })
 
-  const { loading } = useQuery(GET_USER, {
+  const {
+    data: userData,
+    loading,
+    refetch,
+  } = useQuery(GET_USER, {
     skip: !id,
     variables: {
       id,
     },
     fetchPolicy: 'network-only',
+    notifyOnNetworkStatusChange: true,
     onCompleted: (res: any) => {
       setState({
         ...state,
@@ -114,40 +135,24 @@ const UserEdit: FC<any & WrappedComponentProps> = (props: any) => {
     },
   })
 
-  const { data: orgData } = useQuery(GET_ORG, {
-    variables: {
-      pageSize: 999,
-    },
-  })
-
-  const [saveUser, { loading: saveUserLoading }] = useMutation(SAVE_USER, {
-    onCompleted(res: any) {
-      if (onSave) {
-        onSave()
-      }
-
-      setState({
-        ...state,
-        id: state.id ?? res?.saveUser?.id,
-        message: 'success',
-      })
-    },
-
-    onError: () => {
-      setState({
-        ...state,
-        message: 'error',
-      })
-    },
-  })
-
   const { loading: loadingRoles, data: dataRoles } = useQuery(GET_ROLES)
+
+  const [saveUser, { loading: saveUserLoading }] = useMutation(SAVE_USER)
+
+  const [deleteUser, { loading: deleteUserLoading }] = useMutation(DELETE_USER)
 
   const [getCostCenter, { data: dataCostCenter, called }] =
     useLazyQuery(GET_COST)
 
+  const onMutationError = () => {
+    setState({
+      ...state,
+      message: 'error',
+    })
+  }
+
   const handleSaveUser = () => {
-    const variables: any = {
+    const variables = {
       id: state.id,
       clId: state.clId,
       userId: state.userId,
@@ -159,26 +164,62 @@ const UserEdit: FC<any & WrappedComponentProps> = (props: any) => {
       canImpersonate: state.canImpersonate,
     }
 
-    if (state.id) {
-      variables.id = state.id
-    }
-
     saveUser({
       variables,
     })
+      .then((res) => {
+        if (onSave) {
+          onSave()
+        }
+
+        setState({
+          ...state,
+          id: state.id ?? res?.data?.saveUser?.id,
+          message: 'success',
+        })
+      })
+      .catch(onMutationError)
   }
 
-  const handleClear = () => {
-    setState({
-      ...state,
-      orgId: null,
-      costId: null,
+  const handleDeleteUser = () => {
+    const variables = {
+      id: state.id,
+      userId: state.userId,
+      email: state.email,
+    }
+
+    deleteUser({
+      variables,
     })
+      .then((res) => {
+        if (onSave) {
+          onSave()
+        }
+
+        setState({
+          ...state,
+          roleId: null,
+          orgId: null,
+          costId: null,
+          id: state.id ?? res?.data?.deleteUser?.id,
+          message: 'success',
+        })
+        refetch()
+      })
+      .catch(onMutationError)
   }
 
-  const optionsOrg = parseOptions(orgData?.getOrganizations) ?? []
-  const optionsCost =
-    parseOptions(dataCostCenter?.getCostCentersByOrganizationId) ?? []
+  const optionsCost = parseOptions(
+    dataCostCenter?.getCostCentersByOrganizationId
+  )
+
+  const roleOptions =
+    dataRoles?.listRoles?.map((role: any) => {
+      return {
+        value: role.id,
+        label: role.name,
+      }
+    }) ?? []
 
   if (state.costId && !called) {
     getCostCenter({
@@ -188,126 +229,152 @@ const UserEdit: FC<any & WrappedComponentProps> = (props: any) => {
     })
   }
 
+  if (loading || loadingRoles) {
+    return (
+      <div className="w-100 pt6">
+        <Spinner />
+      </div>
+    )
+  }
+
   return (
     <div className="w-100 pt6">
-      {showName && <div className="mb5">{state.name}</div>}
-      {showEmail && <div className="mb5">{state.email}</div>}
-      <div className="mb5 w-80">
-        <Dropdown
-          label={intl.formatMessage(messages.role)}
-          disabled={loadingRoles || loading}
-          options={
-            dataRoles?.listRoles?.map((role: any) => {
-              return {
-                value: role.id,
-                label: role.name,
-              }
-            }) ?? []
-          }
-          value={state.roleId}
-          onChange={(_: any, v: any) => {
-            setState({ ...state, roleId: v })
-          }}
-        />
-      </div>
-
-      {dataRoles && (
-        <div className="mb5 w-100">
-          <div className="flex">
-            <div className="mr2 w-80">
-              <Dropdown
-                label={intl.formatMessage(messages.organization)}
-                options={optionsOrg}
-                value={state.orgId}
-                onChange={(_: any, orgId: any) => {
-                  setState({ ...state, orgId, costId: null })
-                  getCostCenter({
-                    variables: {
-                      id: orgId,
-                    },
-                  })
-                }}
-              />
-            </div>
-            {state.orgId && (
-              <div className="mr2 mt2 w-20 mt6">
-                <ButtonWithIcon
-                  icon={remove}
-                  variation="danger-tertiary"
-                  onClick={() => {
-                    handleClear()
-                  }}
-                />
-              </div>
-            )}
+      <Card>
+        <h5 className="t-heading-5 fw4 mt1 mb6">
+          {formatMessage(messages.b2bInfo)}
+        </h5>
+        {showName && <div className="mb5">{state.name}</div>}
+        {showEmail && <div className="mb5">{state.email}</div>}
+        {roleOptions.length > 1 && (
+          <div className="mb5 w-80">
+            <Dropdown
+              label={formatMessage(messages.role)}
+              options={roleOptions}
+              value={state.roleId}
+              onChange={(_: any, v: string) => {
+                setState({
+                  ...state,
+                  roleId: v,
+                })
+              }}
+            />
           </div>
-        </div>
-      )}
+        )}
 
-      {state.orgId && (
-        <div className="mb5 w-80">
-          <Dropdown
-            label={intl.formatMessage(messages.costCenter)}
-            options={optionsCost}
-            value={state.costId}
-            onChange={(_: any, costId: any) => {
-              setState({ ...state, costId })
-            }}
-          />
-        </div>
-      )}
+        {dataRoles && state.roleId && (
+          <div className="mb5 w-100">
+            <div className="flex">
+              <div className="mr2 w-80">
+                <label className="h-100">
+                  <span className="db mt0 mb3 c-on-base t-small">
+                    {formatMessage(messages.organization)}
+                  </span>
+                  <OrganizationsAutocomplete
+                    organizationId={state.orgId}
+                    onChange={(event) => {
+                      if (event.value === state.orgId) {
+                        return
+                      }
 
-      {state.orgId && !state.costId ? (
-        <div className="mv4">
-          <Alert type="error">{intl.formatMessage(messages.alertPick)}</Alert>
-        </div>
-      ) : null}
+                      setState({
+                        ...state,
+                        orgId: event.value,
+                        costId: null,
+                      })
+                      if (!event.value) return
+                      getCostCenter({
+                        variables: {
+                          id: event.value,
+                        },
+                      })
+                    }}
+                  />
+                </label>
+              </div>
+            </div>
+          </div>
+        )}
 
-      <div className="mv4 flex justify-between">
-        {showCancel && onCancel && (
+        {state.orgId && (
+          <div className="mb5 w-80">
+            <Dropdown
+              label={formatMessage(messages.costCenter)}
+              options={optionsCost}
+              value={state.costId}
+              onChange={(_: any, costId: string) => {
+                setState({ ...state, costId })
+              }}
+            />
+          </div>
+        )}
+
+        {state.orgId && !state.costId ? (
+          <div className="mv4">
+            <Alert type="error">{formatMessage(messages.alertPick)}</Alert>
+          </div>
+        ) : null}
+
+        <div className="mv4 flex justify-start">
+          {showCancel && onCancel && (
+            <div className="mr3">
+              <Button
+                variation="tertiary"
+                disabled={loading}
+                onClick={() => {
+                  onCancel()
+                }}
+              >
+                {formatMessage(messages.cancel)}
+              </Button>
+            </div>
+          )}
           <Button
-            variation="tertiary"
-            disabled={loading}
-            collapseLeft
+            variation="primary"
+            disabled={
+              loading ||
+              saveUserLoading ||
+              deleteUserLoading ||
+              !state.name ||
+              !state.email ||
+              !state.orgId ||
+              !state.costId ||
+              !state.roleId
+            }
             onClick={() => {
-              onCancel()
+              handleSaveUser()
             }}
           >
-            {intl.formatMessage(messages.cancel)}
+            {formatMessage(messages.save)}
           </Button>
+          {userData?.getUser?.roleId && (
+            <div className="ml3">
+              <Button
+                variation="danger"
+                disabled={loading || saveUserLoading || deleteUserLoading}
+                onClick={() => {
+                  handleDeleteUser()
+                }}
+              >
+                {formatMessage(messages.delete)}
+              </Button>
+            </div>
+          )}
+        </div>
+        {state.message && (
+          <Alert
+            type={state.message}
+            onClose={() => {
+              setState({ ...state, message: null })
+            }}
+          >
+            {state.message === 'success'
+              ? formatMessage(messages.success)
+              : formatMessage(messages.error)}
+          </Alert>
         )}
-        <Button
-          variation="primary"
-          disabled={
-            loading ||
-            (state.orgId && !state.costId) ||
-            saveUserLoading ||
-            !state.name ||
-            !state.email ||
-            !state.roleId
-          }
-          collapseRight
-          onClick={() => {
-            handleSaveUser()
-          }}
-        >
-          {intl.formatMessage(messages.save)}
-        </Button>
-      </div>
-      {state.message && (
-        <Alert
-          type={state.message}
-          onClose={() => {
-            setState({ ...state, message: null })
-          }}
-        >
-          {state.message === 'success'
-            ? intl.formatMessage(messages.success)
-            : intl.formatMessage(messages.error)}
-        </Alert>
-      )}
+      </Card>
     </div>
   )
 }
 
-export default injectIntl(UserEdit)
+export default UserEdit
